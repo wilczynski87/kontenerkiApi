5e9e13f495b3ba76747f6ced17e3f555330192c3
Karol <wilczynski87@gmail.com>
2026-01-13 15:08:56 +0100

    clientsPayments controller

--- src/main/kotlin/controller/ListingController.kt
+++ src/main/kotlin/controller/ListingController.kt
@@ -1,14 +1,13 @@
 package com.kontenery.controller
 
-import com.kontenery.library.model.PaymentForFinanceTable
 import com.kontenery.library.model.PaymentsListForFinanceTable
 import com.kontenery.library.model.Product
+import com.kontenery.library.utils.endOfCurrentYear
+import com.kontenery.library.utils.startOfCurrentYear
 import com.kontenery.service.ListingService
-import io.ktor.client.call.body
-import io.ktor.client.request.get
-import io.ktor.client.request.parameter
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.datetime.LocalDate
 
 fun Route.listingRoute(
     listingService: ListingService
@@ -35,8 +34,13 @@ fun Route.listingRoute(
         get("/clientsPayments") {
             val page: Int = call.request.queryParameters["page"]?.toInt() ?: 0
             val size: Int = call.request.queryParameters["size"]?.toInt() ?: 100
+            val fromRaw: String? = call.request.queryParameters["from"]
+            val toRaw: String? = call.request.queryParameters["to"]
 
-            val clientListPayments: List<PaymentsListForFinanceTable> = listingService.clientsFinancesList(page, size)
+            val from: LocalDate = LocalDate.parse(fromRaw ?: LocalDate.startOfCurrentYear().toString())
+            val to: LocalDate = LocalDate.parse(toRaw ?: LocalDate.endOfCurrentYear().toString())
+
+            val clientListPayments: List<PaymentsListForFinanceTable> = listingService.clientsFinancesList(page, size, from, to)
             println("clientListPayments: $clientListPayments")
 
 
