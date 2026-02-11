72553886a0c88afde495c4aa224295f43dde48bb
Karol <wilczynski87@gmail.com>
2026-02-11 10:08:15 +0100

    cors fix

--- src/main/kotlin/Security.kt
+++ src/main/kotlin/Security.kt
@@ -4,25 +4,10 @@ import com.kontenery.service.JwtConfig
 import io.ktor.http.*
 import io.ktor.http.auth.AuthScheme
 import io.ktor.http.auth.HttpAuthHeader
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.CORS
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import kotlinx.serialization.Serializable
-import java.sql.Connection
-import java.sql.DriverManager
-import org.jetbrains.exposed.sql.*
-import org.koin.dsl.module
-import org.koin.ktor.plugin.Koin
-import org.koin.logger.slf4jLogger
-import java.security.MessageDigest
-import java.util.Date
-import kotlin.text.Charsets.UTF_8
 
 fun Application.configureSecurity(jwtConfig: JwtConfig) {
 
